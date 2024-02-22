a54b0570c65e3bb973c1cc03f6bfa938bf792abe
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-02-22 15:09:07 +0100

    center cards div added

--- client/src/pages/addCard.jsx
+++ client/src/pages/addCard.jsx
@@ -1,6 +1,12 @@
 function AddCardPage() {
     return ( 
-        <div>Formulaire</div>
+        <>
+            <div className="w-[40%] ml-[9rem] my-[8rem] h-[60%] z-1 border border-blue-600 absolute  text-white">Quelque chose</div>
+            <div className="flex h-[100%] w-[100%]">
+                <div className="border border-black w-[30%] h-[100%] bg-black"></div>
+                <div></div>
+            </div>
+        </>
      );
 }
 
