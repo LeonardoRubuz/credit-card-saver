06f00e919539a921cffcc09a4167da39fa734247
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-02-23 10:15:18 +0100

    left section background color changed

--- client/src/pages/addCard.jsx
+++ client/src/pages/addCard.jsx
@@ -9,7 +9,7 @@ function AddCardPage() {
                 <BackCard />
             </div>
             <div className="flex h-[100%] w-[100%]">
-                <div className="border border-black w-[30%] h-[100%] bg-black"></div>
+                <div className="border border-black w-[30%] h-[100%] bg-gradient-to-br from-[#230a31] via-[#4e1e48] to-[#1b0828]"></div>
                 <div></div>
             </div>
         </>
