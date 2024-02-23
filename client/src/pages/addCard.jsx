4585bba7bb2f4a9b39c410131b05e384ef8ca24d
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-02-23 11:01:13 +0100

    form design finished

--- client/src/pages/addCard.jsx
+++ client/src/pages/addCard.jsx
@@ -1,4 +1,5 @@
 import BackCard from "../components/backCard";
+import Form from "../components/form";
 import FrontCard from "../components/frontCard";
 
 function AddCardPage() {
@@ -7,10 +8,12 @@ function AddCardPage() {
             <div className="w-[40%] ml-[9rem] my-[8rem] h-[60%] z-1 absolute text-white flex flex-col justify-between gap-6">
                 <FrontCard />
                 <BackCard />
-            </div>
+            </div>  
             <div className="flex h-[100%] w-[100%]">
                 <div className="border border-black w-[30%] h-[100%] bg-gradient-to-br from-[#230a31] via-[#4e1e48] to-[#1b0828]"></div>
-                <div></div>
+                <div className="w-[70%] flex items-center justify-center">
+                    <Form />
+                </div>
             </div>
         </>
      );
