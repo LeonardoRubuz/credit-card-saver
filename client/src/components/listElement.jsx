99d695ee981d52be13498af4a870a2533dc114f2
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-02-26 11:59:44 +0100

    update : list elements match the api datas

--- client/src/components/listElement.jsx
+++ client/src/components/listElement.jsx
@@ -1,12 +1,15 @@
+import { useContext } from "react";
 import Badge from "../components/badge";
+import CardContext from "../contexts/cardContext";
 
 function ListElement() {
+    const card = useContext(CardContext)
     return ( 
         <li className="rounded-md bg-[#1f203a] flex px-7 h-[3.8rem] items-center justify-between">
-            <p className="font-semibold">CVC</p>
-            <p className="font-light text-[0.9rem]">Holder name</p>
-            <p className="font-light text-[0.9rem]">Due Date</p>
-            <p className="font-semibold">Amount of money</p>
+            <p className="font-semibold">{card.cvc}</p>
+            <p className="font-light text-[0.9rem]">{card.holder}</p>
+            <p className="font-light text-[0.9rem] text-center">{card.expiryMonth}-{card.expiryYear}</p>
+            <p className="font-semibold text-center">{card.money}</p>
             <Badge status="Active" />
         </li>
      );
