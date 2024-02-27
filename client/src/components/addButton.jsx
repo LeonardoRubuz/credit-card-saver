09f7dbf75cbd3fc7b28f5f6cb300852a50bbc0b5
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-02-27 14:41:03 +0100

    add-button stylished

--- client/src/components/addButton.jsx
+++ client/src/components/addButton.jsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 
 function AddButton() {
     return ( 
-        <button className="h-[100%]  bg-violet-600">
-            <Link to="/add-card">
-                Add card
+        <button className="h-[90%] w-[8rem] rounded-[30px]  bg-violet-600">
+            <Link to="/add-card" className="flex justify-center gap-4">
+                <div className="bg-white w-6 rounded-[50%] flex items-center justify-center">
+                    <img className="h-4" src="/plus-solid.svg" alt="" />
+                </div>
+                <span>Add card</span>
             </Link>
         </button>
      );
